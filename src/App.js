96cad71f4a931c7388acb2f5cb401a637f3f3ad6
riqza ch <riqzashabbir@gmail.com>
2020-12-23 19:43:02 +0500

    second comit

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ import Home from './Home';
 import About from './About';
 import Dashboard from './contact'
 
-
-// This site has 3 pages, all of which are rendered
-// dynamically in the browser (not server rendered).
-//
-// Although the page does not ever refresh, notice how
-// React Router keeps the URL up to date as you navigate
-// through the site. This preserves the browser history,
-// making sure things like the back button and bookmarks
-// work properly.
-
 export default function App() {
   return (
     <Router>
@@ -36,14 +26,6 @@ export default function App() {
         </ul>
 
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <Home />
@@ -60,6 +42,5 @@ export default function App() {
   );
 }
 
-// You can think of these components as "pages"
-// in your app.
+
 
